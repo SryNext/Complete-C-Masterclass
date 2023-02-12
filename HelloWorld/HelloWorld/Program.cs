926f29f64e0af66ed1ecbb122317b9f09ffb56bb
SryNext <112487716+SryNext@users.noreply.github.com>
2023-02-12 20:51:39 +0100

    Simple Hello World Console App in C#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        //  Einstiegspunkt für das Program
+
+        static void Main(string[] args)
+        {
+            //  Setzt die Hintergrundfarbe der Konsole
+            Console.BackgroundColor= ConsoleColor.DarkGray;
+            //  Setzt die Schriftfarbe der Konsole
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Hallo ...");
+            //  Wartet auf Eingabe einer Taste für weitere Ausführung
+            Console.ReadKey();
+            
+        }
+    }
+}
