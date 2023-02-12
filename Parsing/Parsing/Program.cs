49b666e999885b5256bd1e8be4b8cf75fc4f1df7
SryNext <112487716+SryNext@users.noreply.github.com>
2023-02-12 20:56:51 +0100

    parsing example

---
+++ Parsing/Parsing/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+
+namespace Parsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "15";
+            string my2ndString = "13";
+            int num1 = int.Parse(myString);
+            int num2 = int.Parse(my2ndString);
+            int resultInt = num1+ num2;
+
+            string result = myString+ my2ndString;
+            Console.WriteLine(resultInt);
+            Console.ReadKey();
+        }
+    }
+}
