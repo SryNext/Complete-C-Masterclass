d7e2de55209ea7df0c981d21fb0e1e3568a0488d
SryNext <112487716+SryNext@users.noreply.github.com>
2023-02-12 20:57:20 +0100

    some string examples

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Deklarieren und Initialisieren der Variablen meinName
+            string meinName = "Domi";
+
+            string nachricht = "Mein name ist " + meinName;
+            int laengeDesStrings = nachricht.Length;
+            //  die ToUpper Methode gibt den String in Großschrift aus.
+            string allesGrossGeschrieben = nachricht.ToUpper();
+            string allesKleinGeschrieben = nachricht.ToLower();
+
+            Console.WriteLine(allesKleinGeschrieben);
+            Console.WriteLine(allesGrossGeschrieben);
+            Console.WriteLine(nachricht + " ist {0} Zeichen lang", laengeDesStrings);
+            Console.ReadKey();
+        }
+
+    }
+}
