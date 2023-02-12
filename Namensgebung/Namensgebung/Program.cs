ca3b07242b09b18c919f874a9315e18a5ab99a3e
SryNext <112487716+SryNext@users.noreply.github.com>
2023-02-12 20:56:20 +0100

    naming

---
+++ Namensgebung/Namensgebung/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Namengebung
+{
+    //  Klassennamen sollten immer mit Großbuchstaben beginnen || z.b. ClientActivity
+    class Progam
+    {
+        //  Methodenname sollten nach Möglichkeit Verben sein, wie z.b. CalculateValue
+        //  Methodenargumente werden klein geschrieben
+        static void Main(string[] args)
+        {
+            //  variable heißt wie z.b. itemCount
+            //  nutze userControl statt usrCtr
+
+            //  keine Zahlen bei Variablennamen am Anfang verwenden
+            int num5 = 3;
+
+            //  richtig
+            string myName;
+            int year;
+            bool isSaved;
+
+            /*
+             *  vermeide:
+             *  String myName;
+             *  Int32 lastNum;
+             *  Boolean isSaved;
+             */
+
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
