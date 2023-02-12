7c358c42a535efb6d56c4365e7d7b1d5948bf8d4
SryNext <112487716+SryNext@users.noreply.github.com>
2023-02-12 20:54:55 +0100

    constants

---
+++ Konstanten/Konstanten/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Konstanten
+{
+    class Program
+    {
+        /*  Konstanten ( bleibende Körper ) sind normalerweise Felder welche während
+         *  der Kompilierzeit bekannt sind und danach für die gesamte Laufzeit
+         *  des Programmes nicht verändert werden können. Muss Sofort definiert werden */
+        const double PI = Math.PI;
+        const int weeks = 52, months = 12;
+        const string geburtstag = "17.10.1993";
+
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Mein Geburtstag ist am: " + geburtstag);
+            Console.ReadKey();
+        }
+    }
+}
