dfb437da400fa9d5975c2c90077c4f4954c06ba1
SryNext <112487716+SryNext@users.noreply.github.com>
2023-02-12 20:57:53 +0100

    void method examples

---
+++ Void Methoden/Void Methoden/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace VoidMethode
+{
+    class Program
+    {
+        //  Startpunkt des Programmes
+        static void Main(string[] args)
+        {
+            WriteSomething();
+            WriteSomething();
+            WriteSomething();
+            string myArgument = "Hallo, ich bin super Text";
+            WriteSomethingSpecific(myArgument);
+            Console.ReadKey();
+        }
+
+        //  Zugriffsmodifizierer (static) Rückgabetyp Methodenname(parameter1, parameter2)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("Ich bin eine Methode!");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+
+    }
+}
